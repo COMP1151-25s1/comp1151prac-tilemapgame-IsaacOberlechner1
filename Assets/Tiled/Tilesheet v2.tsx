216a306de43cac8eadad0120bbf26e4fef341ba6
IsaacOberlechner <isaacdoberlechner@gmail.com>
2025-05-22 14:19:11 +1000

    Fixed Tilemap Grid + Blur
    - Added png files from the actuasl Tilemap file (.tsx), not the individual tiles in the package

---
+++ Assets/Tiled/Tilesheet v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tilesheet v2" tilewidth="16" tileheight="16" spacing="1" tilecount="110" columns="11">
+ <image source="../Tilemap Game/kenney_tiny-dungeon/Tilemap/tilemap_packed.png" width="192" height="176"/>
+</tileset>
